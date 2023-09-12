3d60fc54845955f893a436e05ad29be4bc675448
Huyiming2023 <c3huyi00@students.oamk.fi>
2023-09-12 23:30:40 +0300

    bundle N

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello world!
+        </p>
+        <p>
+        I am HuYiming, this is my bundle N work.
+        </p>        
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
